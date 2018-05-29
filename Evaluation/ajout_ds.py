c0f038fa60d315aa35bfbdc64562364befdb14ec
Xavier Pessoles <xpessoles.ptsi@free.fr>
2018-05-29 23:24:53 +0200

    MAJ

---
+++ Evaluation/ajout_ds.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+__author__ = "Xavier Pessoles"
+
+"""
+Ajout des notes d'un DS à partir d'un fichier CSV.
+
+"""
---
+++ Evaluation/ajout_eleves.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+__author__ = "Xavier Pessoles"
+
+""" 
+Ajout des élèves dans la BDD à partir d'un fichier csv de la forme :
+nom, prenom, date de naissance, adresse mail
+"""
