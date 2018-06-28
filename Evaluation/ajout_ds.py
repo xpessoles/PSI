d2f0c5ab01ce020f91552539ea71b0d271f5a56b
Xavier Pessoles <xpessoles.ptsi@free.fr>
2018-06-28 23:58:20 +0200

    MAJ  script

--- Evaluation/ajout_ds.py
+++ Evaluation/ajout_ds.py
@@ -87,29 +87,65 @@ def remplir_bdd(num_ds,annee,competences,nb_questions, bareme, poids, notes,bdd)
             data = data + str(competences[i-1])+'","'
             data = data + str(eleve[nb_questions+1])+'"'
             req = 'INSERT INTO ds VALUES ('+data+')'
-            #print(req)
+            print(req)
             
             
             c.execute(req)
     conn.commit()
     conn.close()
 
-def bilan_ds(num_ds):
-    # On récupère pour chaque eleve : num_question, poids, bareme, note, id_competence
+def bilan_ds(num_ds,bdd,promo):
+    # On récupère le num de chaque élève
+    conn = sqlite3.connect(bdd)
+    c = conn.cursor()
+    req = "SELECT id_eleve FROM eleves WHERE annee_integration="+str(promo)
+    c.execute(req)
+    tab = c.fetchall()
+    id_eleves=[tab[i][0] for i in range(len(tab))]
+    conn.commit()
+    conn.close()
     
+    # On récupère pour chaque eleve : num_question, poids, bareme, note, id_competence
+    notes=[]
+    for id in id_eleves :
+        conn = sqlite3.connect(bdd)
+        c = conn.cursor()
+        req = "SELECT num_question, poids, bareme, note, id_competence FROM ds WHERE id_eleve="+str(id)+" AND num_ds="+str(num_ds)
+        c.execute(req)
+        tab = c.fetchall()
+        conn.commit()
+        conn.close()
+        # On calcule la note :
+        note_gl = 0
+        note_el = 0 
+        for q in tab :
+            
+            note_gl += q[1]*q[2]
+            note = q[3]
+            if q[3]=="n" or q[3]=="":
+                note = 0
+            #print(note_el,type(q[2]),type(note),note)
+            note_el = note_el+ q[2]*note
+        notes.append([id,note_el,note_gl,note_el*20/note_gl])
+
+    
+    
+    return notes
     # On calcule la note
         
     
+promo = 2018
+num_ds = 1
 
-
-nb_questions, bareme, poids, notes, competences = lire_fichier("file_csv")    
-remplir_bdd(num_ds,annee,competences,nb_questions, bareme, poids, notes,bdd)
-bilan_ds(num_ds)
-
+#nb_questions, bareme, poids, notes, competences = lire_fichier(file_csv)    
+#remplir_bdd(num_ds,annee,competences,nb_questions, bareme, poids, notes,bdd)
+tt = bilan_ds(1,bdd,promo)
+for i in tt:
+    print(tt)
 
 """
 (
-	`id_eleve`	INTEGER,
+	`id_eleve`	INTEGER,tt
 	`num_ds`	INTEGER,
 	`annee`	INTEGER,
 	`nb_questions`	INTEGER,
